dc202843d991f24cbd1a63afbadb801bcc96f79a
BorisKiselev <hugh.dancy@icloud.com>
2022-08-17 14:27:36 +0700

    Initial Commit

---
+++ HW26_KiselevBoris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW26_KiselevBoris
+//
+//  Created by Борис Киселев on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
